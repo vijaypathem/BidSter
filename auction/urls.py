4b061906c50d962cd72ebba4cf140177a98e4264
vijay pathem <vijaypathem4@gmail.com>
2023-10-04 21:31:17 +0530

    stage 1

--- auction/urls.py
+++ auction/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
-from .views import view_users, create_users, user_details
+from .views import (
+    view_users,
+    create_users,
+    user_details,
+    view_auction_bids,
+    view_auctions,
+    view_user_bids,
+    create_bids,
+    auction_details,
+)
 
 urlpatterns = [
     # view all users
@@ -8,4 +17,14 @@ urlpatterns = [
     path("users/create/", create_users),
     # view user details, delete user , update user
     path("users/<int:pk>/", user_details),
+    # view bids for an action
+    path("auctions/bids/<int:pk>/", view_auction_bids),
+    # view user bids
+    path("users/me/bids/", view_user_bids),
+    # create bids
+    path("auctions/create-bid/", create_bids),
+    # view auctions and admin able to create an auction
+    path("auctions/", view_auctions),
+    # auction details, update an auction , delete auction
+    path("auctions/<int:pk>/", auction_details),
 ]
--- auction/views.py
+++ auction/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, get_list_or_404
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
@@ -43,6 +43,7 @@ def user_details(request, pk):
     if request.method == "DELETE":
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 # admin able to create users
@@ -59,20 +60,79 @@ def create_users(request):
 
 
 # all users able to see all the auctions
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 @permission_classes(())
 def view_auctions(request):
     if request.method == "GET":
         auctions = Auction.objects.all()
         serializer = AuctionSerializer(auctions, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if request.user.is_staff():
+        if request.method == "POST":
+            serializer = AuctionSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-# manager able to see all bids
+# admin able to do CRUD operations on Auctions
+@api_view(["GET", "PUT", "DELETE"])
+@permission_classes((IsAuthenticated,))
+def auction_details(request, pk):
+    auction = get_object_or_404(Auction, pk=pk)
+    if request.method == "GET":
+        serializer = AuctionSerializer(auction)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    if not request.user.is_staff():
+        return Response(
+            {"detail": "you are not allowed"}, status=status.HTTP_400_BAD_REQUEST
+        )
+    if request.method == "PUT":
+        serializer = AuctionSerializer(auction, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+    if request.method == "DELETE":
+        auction.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+# manager able to see all bids of an auction
+@api_view(["GET"])
+@permission_classes((IsAdminUser,))
+def view_auction_bids(request, pk):
+    if request.method == "GET":
+        auction_bids = get_list_or_404(Bid, auction=pk)
+        serializer = BidSerializer(auction_bids, many=True)
+        if serializer.data:
+            return Response(serializer.data)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 # user able to see all his bids
+@api_view(["GET"])
+@permission_classes((IsAuthenticated,))
+def view_user_bids(request):
+    if request.method == "GET":
+        user_bids = get_list_or_404(Bid, bidder=request.user)
+        serializer = BidSerializer(user_bids, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-# admin able to do CRUD operations on Auctions
+# users able to bid auction
+@api_view(["POST"])
+@permission_classes((IsAuthenticated,))
+def create_bids(request):
+    if request.method == "POST":
+        serializer = BidSerializer(data=request.data)
+        serializer.initial_data["bidder"] = request.user.id
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
